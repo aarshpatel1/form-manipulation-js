4981e86fcd1963a55d977bccb6e3a10d7880b823
aarshpatel1 <aarsh18patel@gmail.com>
2024-10-04 11:04:08 +0530

    modify record bug solve

--- script.js
+++ script.js
@@ -4,22 +4,29 @@ const addData = document.getElementById("add-data");
 const record = document.getElementById("records-row");
 
 var myLocalStorage = JSON.parse(localStorage.getItem("form-data")) || [];
+var isEditing = false;  // Track if we are in edit mode
+var editIndex = -1;     // Store the index of the current record being edited
 
 function saveData() {
     localStorage.setItem("form-data", JSON.stringify(myLocalStorage));
 }
 
 addData.addEventListener("click", function () {
-    myLocalStorage.push({
-        "id": generateUniqueID(),
-        "email": userEmail.value,
-        "password": userPassword.value,
-        "time": formatDateTime()
-    });
+    if (isEditing) {
+        // Modify the existing record if in edit mode
+        modifyData(editIndex);
+    } else {
+        // Add new data if not in edit mode
+        myLocalStorage.push({
+            "id": generateUniqueID(),
+            "email": userEmail.value,
+            "password": userPassword.value,
+            "time": formatDateTime()
+        });
+    }
     saveData();
     updateData();
-    userEmail.value = "";
-    userPassword.value = "";
+    clearForm();
 });
 
 function updateData() {
@@ -46,15 +53,19 @@ function loadData(index) {
     const currentRecord = myLocalStorage[index];
     userEmail.value = currentRecord.email;
     userPassword.value = currentRecord.password;
+    isEditing = true;       // Set to edit mode
+    editIndex = index;       // Store the index of the current record
+    addData.innerText = "Modify";  // Change the button text to indicate edit mode
 }
 
 function modifyData(index) {
-    const currentRecord = myLocalStorage[index]
-    currentRecord.email = userEmail.value
-    currentRecord.password = userPassword.value
-    currentRecord.time = formatDateTime()
-    saveData()
-    updateData()
+    const currentRecord = myLocalStorage[index];
+    currentRecord.email = userEmail.value;
+    currentRecord.password = userPassword.value;
+    currentRecord.time = formatDateTime();
+    isEditing = false;  // Exit edit mode
+    editIndex = -1;     // Reset the edit index
+    addData.innerText = "Add Data";  // Reset button text
 }
 
 function deleteData(index) {
@@ -80,6 +91,13 @@ function formatDateTime() {
     const seconds = String(now.getSeconds()).padStart(2, '0');
 
     return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+}
+
+function clearForm() {
+    userEmail.value = "";
+    userPassword.value = "";
+    isEditing = false;  // Reset edit mode after saving
+    addData.innerText = "Add Data";  // Reset button text
 }
 
 updateData();
